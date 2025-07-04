0c5464587626693cbfc35892feca176f97655e8f
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-07-04 19:26:37 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,18 @@
+import json 
 
+with open('students.json') as jsonfile:
+
+    json_str = jsonfile.read()
+
+    data = json.loads(json_str) 
+
+    students = sorted(data,key = lambda x : int(x['score']))
+
+    for i, student in enumerate(students, start=1):
+        student['rank'] = i
+
+    json_str = json.dumps(students,indent=4)
+
+with open('rating.json','w') as jsonfile:
+
+    jsonfile.write(json_str)
